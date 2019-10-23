531238c942b2fbf89f4c815fce7ac6689d6e4273
Gavrilenko <gavrilenko.georgi@gmail.com>
2019-10-23 15:29:39 +0300

    Add part3a theory exercises
    Node.js and express setup
    Various routes defined for adding, updating and deleting data.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ console.log(`Hello world`)
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 let notes = [
   {
@@ -30,6 +33,55 @@ app.get('/', (req, res) => {
 
 app.get('/notes', (req, res) => {
   res.json(notes)
+})
+
+app.get('/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => {
+    console.log(note.id, typeof note.id, id, typeof id, note.id === id)
+    return note.id === id
+  })
+  // console.log(`Note here`, note)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'Content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
 })
 
 const PORT = 3001
