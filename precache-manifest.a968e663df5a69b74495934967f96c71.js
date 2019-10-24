e97d1b271b5b5bdc5a21c94732b84324b1f91eae
Gavrilenko <gavrilenko.georgi@gmail.com>
2019-10-24 13:04:04 +0300

    Add front-end

---
+++ precache-manifest.a968e663df5a69b74495934967f96c71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b40e54288d5db4dd22889587b0d7af62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57abc10e21e22236b247",
+    "url": "/static/css/main.7ea0ab29.chunk.css"
+  },
+  {
+    "revision": "db27d2ba21d49a45f23b",
+    "url": "/static/js/2.1b1f1e86.chunk.js"
+  },
+  {
+    "revision": "57abc10e21e22236b247",
+    "url": "/static/js/main.1cbc316b.chunk.js"
+  },
+  {
+    "revision": "10d788ab4011930941ba",
+    "url": "/static/js/runtime-main.10e5d2d1.js"
+  }
+]);
