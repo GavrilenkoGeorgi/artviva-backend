4f4849ff0ef8317d68179e51bf6a20fd7fdab64a
Gavrilenko <gavrilenko.georgi@gmail.com>
2019-10-24 13:44:42 +0300

    uibuild

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ app.post('/notes', (request, response) => {
   response.json(note)
 })
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
