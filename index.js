4a04b7d483b83936c413ee7e334bbcf8fe0ef576
Gavrilenko <gavrilenko.georgi@gmail.com>
2019-10-24 12:51:14 +0300

    Add cors

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-console.log(`Hello world`)
-
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
+app.use(cors())
 app.use(bodyParser.json())
 
 let notes = [
