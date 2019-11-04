8d85686c06f473dd91511393cc4023bb86cfcecb
Gavrilenko <gavrilenko.georgi@gmail.com>
2019-11-04 15:39:28 +0200

    Add Note model
    Note now is a separate model

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const Note = require('./models/note')
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -10,47 +12,20 @@ app.use(bodyParser.json())
 // in the build directory
 app.use(express.static(__dirname + '/build'))
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
-  }
-]
-
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes.map(note => note.toJSON()))
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => {
-    console.log(note.id, typeof note.id, id, typeof id, note.id === id)
-    return note.id === id
+  Note.findById(request.params.id).then(note => {
+    response.json(note.toJSON())
   })
-  // console.log(`Note here`, note)
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -60,35 +35,28 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({
       error: 'Content missing'
     })
   }
 
-  const note = {
+  const note = new Note ({
     content: body.content,
     important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
+    date: new Date()
+  })
 
-  notes = notes.concat(note)
 
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote.toJSON())
+  })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
