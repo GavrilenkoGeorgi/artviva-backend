fd2a2dc13fa696022d7e51f83fd27ea733ad61fe
Gavrilenko <gavrilenko.georgi@gmail.com>
2019-11-05 14:17:18 +0200

    Add error handling
    In case of non existent id 404 error is returned
    If id is present but invalid 400 bad request
    is returned

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const Note = require('./models/note')
 
-app.use(cors())
-app.use(bodyParser.json())
-
 // Run the app by serving the static files
 // in the build directory
 app.use(express.static(__dirname + '/build'))
+app.use(bodyParser.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
@@ -22,17 +21,39 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note.toJSON())
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note.toJSON())
+      } else {
+        response.status(404).end()
+      }
   })
+  .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
-  response.status(204).end()
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote.toJSON())
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -55,6 +76,23 @@ app.post('/api/notes', (request, response) => {
     response.json(savedNote.toJSON())
   })
 })
+
+// handler of requests with unknown endpoint
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'Unknown endpoint.' })
+}
+app.use(unknownEndpoint)
+
+// Middleware error handler
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind === 'objectId') {
+    return response.status(400).send({ error: 'malformed id' })
+  }
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
--- models/note.js
+++ models/note.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
-
 const url = process.env.MONGODB_URI
+
+mongoose.set('useFindAndModify', false)
 
 console.log('connecting to', url)
 
