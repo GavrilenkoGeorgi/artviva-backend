2cc175fedc238df673e86697ffb0b1d961e0afde
Gavrilenko <gavrilenko.georgi@gmail.com>
2019-11-08 20:08:25 +0200

    Add ESlint

--- models/note.js
+++ models/note.js
@@ -1,18 +1,4 @@
 const mongoose = require('mongoose')
-/*
-const url = process.env.MONGODB_URI
-
-mongoose.set('useFindAndModify', false)
-
-console.log('connecting to', url)
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  }) */
 
 const noteSchema = new mongoose.Schema({
   content:  {
