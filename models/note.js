8d28712c23d165adbd1485725f8fcda64adba472
Gavrilenko <gavrilenko.georgi@gmail.com>
2019-11-14 12:50:37 +0200

    Add refs across collections
    User objects now also contain the contents of the user's notes,
    and not just their id and notes contain author data

--- models/note.js
+++ models/note.js
@@ -7,7 +7,11 @@ const noteSchema = new mongoose.Schema({
     required: true
   },
   date: Date,
-  important: Boolean
+  important: Boolean,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 noteSchema.set('toJSON', {
