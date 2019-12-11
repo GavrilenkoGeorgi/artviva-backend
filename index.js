f95746dcc6aa9d6c90feba737a91dcd8e616c926
Gavrilenko <gavrilenko.georgi@gmail.com>
2019-12-11 19:20:47 +0200

    fix port

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const http = require('http')
 
 const server = http.createServer(app)
 
-server.listen(process.env.PORT || 5000, () => {
+server.listen(process.env.PORT || 3003, () => {
   console.log(`Server is running on port ${process.env.PORT}`)
 })
