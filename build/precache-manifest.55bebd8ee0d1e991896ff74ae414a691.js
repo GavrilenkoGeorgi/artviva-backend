42fe48f6e97ddd7530d9bd7ff4d88ce2b8889b13
Gavrilenko <gavrilenko.georgi@gmail.com>
2020-03-04 17:33:27 +0200

    Build UI

---
+++ build/precache-manifest.55bebd8ee0d1e991896ff74ae414a691.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6176475b3e23179bdd351628bc8573ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40dd8f586fe34d22943d",
+    "url": "/static/css/main.32e423c8.chunk.css"
+  },
+  {
+    "revision": "75b1379c165297fa6931",
+    "url": "/static/js/2.bc78243e.chunk.js"
+  },
+  {
+    "revision": "1617546df35d10078598b1a7d486f8f7",
+    "url": "/static/js/2.bc78243e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "40dd8f586fe34d22943d",
+    "url": "/static/js/main.8fe68115.chunk.js"
+  },
+  {
+    "revision": "d5be5c5e58bcb3c9f1c8",
+    "url": "/static/js/runtime-main.3c18a399.js"
+  },
+  {
+    "revision": "b57d017b27b363bf39b966cf7d815098",
+    "url": "/static/media/edit.b57d017b.svg"
+  },
+  {
+    "revision": "585f4da8f4cc2775e24a968ca77abb77",
+    "url": "/static/media/facebook.585f4da8.svg"
+  },
+  {
+    "revision": "3c1429337a957818e44ad7b3661bc15f",
+    "url": "/static/media/instagram.3c142933.svg"
+  },
+  {
+    "revision": "2fe47c5ea44f88f125820652f377100a",
+    "url": "/static/media/love-song.2fe47c5e.svg"
+  },
+  {
+    "revision": "1908464cb90b79dae6cade513565de63",
+    "url": "/static/media/origami.1908464c.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.405c0e1de3ff1fbdb57d647d3d95d938.js"
+  "/precache-manifest.55bebd8ee0d1e991896ff74ae414a691.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
