5aa8b92269d2fe5608e1b40aa8ac690a0747f892
Gavrilenko <gavrilenko.georgi@gmail.com>
2020-03-12 20:33:59 +0200

    Change static.json settings

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const app = require('./app')
 const http = require('http')
-const path = require('path')
 
 const server = http.createServer(app)
 
@@ -8,10 +7,3 @@ server.listen(process.env.PORT || 5000, () => {
 	console.log(`Server is running on port ${process.env.PORT}`)
 })
 
-app.get('/*', function(req, res) {
-	res.sendFile(path.join(__dirname, 'build/index.html'), function(err) {
-		if (err) {
-			res.status(500).send(err)
-		}
-	})
-})
