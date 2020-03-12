53c91f8710fc8513fc0c5f64a1f9e945fc8746cb
Gavrilenko <gavrilenko.georgi@gmail.com>
2020-03-12 20:12:56 +0200

    Add default redirect to index.html

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const app = require('./app')
 const http = require('http')
+const path = require('path')
 
 const server = http.createServer(app)
 
 server.listen(process.env.PORT || 5000, () => {
 	console.log(`Server is running on port ${process.env.PORT}`)
+})
+
+app.get('/*', function(req, res) {
+	res.sendFile(path.join(__dirname, 'build/index.html'), function(err) {
+		if (err) {
+			res.status(500).send(err)
+		}
+	})
 })
