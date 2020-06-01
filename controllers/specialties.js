1aa072e98e8304cd4f5fdb3b60122c59ebd81d16
Gavrilenko <gavrilenko.georgi@gmail.com>
2020-06-01 14:49:31 +0300

    Change auth check

--- controllers/specialties.js
+++ controllers/specialties.js
@@ -1,77 +1,63 @@
 const specialtiesRouter = require('express').Router()
 const Specialty = require('../models/specialty')
-const jwt = require('jsonwebtoken')
-const { getTokenFromReq } = require('../utils/getTokenFromReq')
+// const jwt = require('jsonwebtoken')
+// const { getTokenFromReq } = require('../utils/getTokenFromReq')
+const { checkAuth } = require('../utils/checkAuth')
 
 // create new specialty
 specialtiesRouter.post('/', async (request, response, next) => {
-
-	const token = getTokenFromReq(request)
-
 	try {
-		const decodedToken = jwt.verify(token, process.env.SECRET)
-		if (!token || !decodedToken.id) {
-			return response.status(401).json({
-				error: 'Неаутентифіковані. Маркер відсутній або недійсний.'
+		if (checkAuth(request)) {
+
+			const { title, cost } = { ...request.body }
+			if (!title || !cost ) {
+				return response.status(400).send({
+					error: 'Деякі обов\'язкові поля даних відсутні.'
+				})
+			}
+
+			// check if specialty with this title already exists
+			const existingSpecialty = await Specialty.findOne({ title })
+			if (existingSpecialty) return response.status(400).json({
+				message: 'Спеціальність вже існує.',
+				cause: 'title'
 			})
+
+			const specialty = new Specialty(request.body)
+			await specialty.save()
+
+			response.status(200).send(specialty.toJSON())
 		}
-
-		const { title, cost } = { ...request.body }
-
-		if (!title || !cost ) {
-			return response.status(400).send({
-				error: 'Деякі обов\'язкові поля даних відсутні.'
-			})
-		}
-
-		// check if specialty with this title already exists
-		const existingSpecialty = await Specialty.findOne({ title })
-		if (existingSpecialty) return response.status(400).json({
-			message: 'Спеціальність вже існує.',
-			cause: 'title'
-		})
-
-		const specialty = new Specialty(request.body)
-		await specialty.save()
-
-		return response.status(200).send(specialty.toJSON())
-
 	} catch (exception) {
 		next(exception)
 	}
 })
 
 // get all specialties
-specialtiesRouter.get('/', async (request, response) => {
-	const specialties = await Specialty
-		.find({})
-		// .populate('teachers', { name: 1 })
-	return response.send(specialties.map(specialty => specialty.toJSON()))
+specialtiesRouter.get('/', async (request, response, next) => {
+	try {
+		const specialties = await Specialty
+			.find({})
+		response.status(200)
+			.send(specialties.map(specialty => specialty.toJSON()))
+	} catch (exception) {
+		next(exception)
+	}
 })
-
 
 // delete specialty
 specialtiesRouter.delete('/:id', async (request, response, next) => {
-
-	const token = getTokenFromReq(request)
-
 	try {
-		const decodedToken = jwt.verify(token, process.env.SECRET)
-		if (!token || !decodedToken.id) {
-			return response.status(401).send({
-				error: 'Неаутентифіковані. Маркер відсутній або недійсний.'
-			})
+		if (checkAuth(request)) {
+			const specialty = await Specialty.findById(request.params.id)
+
+			if (!specialty) {
+				return response.status(404).send({ message: 'Спеціальність з цім ID не знайдена.' })
+			} else if (specialty.schoolClasses.length > 0)
+				return response.status(405).send({ message: 'Ви повинні видалити всі класи, які використовують цю спеціальність.' })
+			await Specialty.findByIdAndRemove(specialty.id)
+			response.status(204).end()
 		}
-
-		const specialty = await Specialty.findById(request.params.id)
-
-		if (!specialty) {
-			return response.status(404).send({ error: 'Спеціальність з цім ID не знайдена.' })
-		}
-
-		await Specialty.findByIdAndRemove(specialty.id)
-		return response.status(204).end()
-
 	} catch (exception) {
 		next(exception)
 	}
@@ -80,9 +66,11 @@ specialtiesRouter.delete('/:id', async (request, response, next) => {
 // update specialty details
 specialtiesRouter.put('/:id', async (request, response, next) => {
 	try {
-		const updatedSpecialty = await Specialty
-			.findByIdAndUpdate(request.params.id, { ...request.body }, { new: true })
-		return response.status(200).json(updatedSpecialty.toJSON())
+		if (checkAuth(request)) {
+			const updatedSpecialty = await Specialty
+				.findByIdAndUpdate(request.params.id, { ...request.body }, { new: true })
+			response.status(200).json(updatedSpecialty.toJSON())
+		}
 	} catch (exception) {
 		next(exception)
 	}
