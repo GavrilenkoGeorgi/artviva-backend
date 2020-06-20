8958084d6ebf4658b56142e7e8ea3fe27fc39dea
Gavrilenko <gavrilenko.georgi@gmail.com>
2020-06-20 15:20:01 +0300

    Modify middleware error handler

--- utils/middleware.js
+++ utils/middleware.js
@@ -21,11 +21,16 @@ const errorHandler = (error, request, response, next) => {
 		})
 	} else if (error.name === 'ValidationError') {
 		return response.status(400).json({
-			message: `Помилка перевірки: ${error.message}`,
+			message: `Помилка перевірки: ${error.message}`
 		})
 	} else if (error.name === 'JsonWebTokenError') {
 		return response.status(401).json({
 			message: 'Неаутентифіковані. Маркер відсутній або недійсний.'
+		})
+	} else if (error.name === 'MongoError' && error.codeName === 'DuplicateKey') {
+		const value = error.keyValue
+		return response.status(409).json({
+			message: `Це значення має бути унікальним: "${value[Object.keys(value)[0]]}"`
 		})
 	}
 
