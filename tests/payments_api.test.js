dc19f70d4771f1af180e2618d59c771563005204
Gavrilenko <gavrilenko.georgi@gmail.com>
2020-10-07 13:44:09 +0300

    Add payment controller tests

--- tests/payments_api.test.js
+++ tests/payments_api.test.js
@@ -1,27 +1,14 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
-// const Payment = require('../models/payment')
+const Payment = require('../models/payment')
 const helper = require('./test_helpers/payment_helper')
 const app = require('../app')
+const PaymentDescr = require('../models/paymentDescr')
+const { getPaymentDataFromString } = require('../utils/processPaymentDescr')
 const api = supertest(app)
 
 let token
 const route = '/api/payment'
-
-/*
-beforeAll(async (done) => {
-	await User.deleteMany({})
-	supertest(app)
-		.post('/api/users')
-		.send({
-			...helper.newUser,
-			teacher: null
-		})
-		.end((err, response) => {
-			activationUUID = response.body.testUUID
-			done()
-		})
-})*/
 
 beforeAll((done) => {
 	supertest(app)
@@ -65,7 +52,7 @@ describe('Payment controller', () => {
 			.expect(502)
 	})
 
-	test('fails to get a list of payments if unathorized', async () => {
+	test('fails to get a list of payments if unauthorized', async () => {
 		const date = new Date()
 
 		await api
@@ -86,6 +73,103 @@ describe('Payment controller', () => {
 			.send({ date_from: date_from.valueOf(), date_to: date_to.valueOf() })
 			.expect(200)
 			.expect('Content-Type', /application\/json/)
+	})
+})
+
+describe('Backup payment list in DB', () => {
+	beforeEach(async () => {
+		await Payment.deleteMany({})
+		await PaymentDescr.deleteMany({})
+
+		const paymentDescriptions = []
+		helper.samplePayments.forEach(payment => {
+			paymentDescriptions.push(getPaymentDataFromString(payment.description, 'uk-UA'))
+		})
+
+		const descriptions = paymentDescriptions.map(descr => new PaymentDescr(descr))
+		const descrToSave = descriptions.map(descr => descr.save())
+		await Promise.all(descrToSave)
+
+		const descrInDb = await PaymentDescr.find({})
+
+		const payments = helper.samplePayments
+			.map((payment, index) => new Payment({ ...payment, paymentDescr: descrInDb[index].id }))
+
+		const samplePayments = payments.map(payment => payment.save())
+		await Promise.all(samplePayments)
+	})
+
+	test('fails with status code 401 to return list of descriptions without authorization', async () => {
+		await api
+			.get(`${route}/descr`)
+			.expect(401)
+	})
+
+	test('returns all payments descriptions', async () => {
+		await api
+			.get(`${route}/descr`)
+			.set('Authorization', `Bearer ${token}`)
+			.expect(200)
+			.expect('Content-Type', /application\/json/)
+			.then(response => {
+				expect(response.body.length).toBe(helper.samplePayments.length)
+			})
+	})
+
+	test('fails to return JSON with status code 401 without authorization', async () => {
+		await api
+			.get(`${route}/list`)
+			.expect(401)
+	})
+
+	test('is returned as JSON', async () => {
+		await api
+			.get(`${route}/list`)
+			.set('Authorization', `Bearer ${token}`)
+			.expect(200)
+			.expect('Content-Type', /application\/json/)
+	})
+
+	test('fails to get payment by ID with status code 401 without authorization', async () => {
+		const [ payment ] = await helper.paymentsInDb()
+
+		await api
+			.get(`${route}/${payment.id}`)
+			.expect(401)
+	})
+
+	test('fails to get payment by ID with status code 404 with nonexistent ID', async () => {
+		const validId = await helper.validNonExistingId()
+
+		await api
+			.get(`${route}/${validId}`)
+			.set('Authorization', `Bearer ${token}`)
+			.expect(404)
+			.then(response => {
+				expect(response.body.error).toContain('Платежу із цим ідентифікатором не знайдено')
+			})
+	})
+
+	test('fails to get payment by ID with status code 400 if ID is invalid', async () => {
+		const invalidId = '5a3d5da59070081a82a3445'
+
+		await api
+			.get(`${route}/${invalidId}`)
+			.set('Authorization', `Bearer ${token}`)
+			.expect(400)
+	})
+
+	test('gets single payment by given ID', async () => {
+		const [ payment ] = await helper.paymentsInDb()
+
+		await api
+			.get(`${route}/${payment.id}`)
+			.set('Authorization', `Bearer ${token}`)
+			.expect(200)
+			.expect('Content-Type', /application\/json/)
+			.then(response => {
+				expect(response.body.id).toBe(payment.id)
+			})
 	})
 })
 
--- tests/test_helpers/payment_helper.js
+++ tests/test_helpers/payment_helper.js
@@ -1,5 +1,7 @@
 const { v4: uuidv4 } = require('uuid')
-// const Payment = require('../../models/payment')
+const Payment = require('../../models/payment')
+const PaymentDescr = require('../../models/paymentDescr')
+const { getPaymentDataFromString } = require('../../utils/processPaymentDescr')
 
 const singlePayment = {
 	action: 'pay',
@@ -12,44 +14,108 @@ const singlePayment = {
 	result_url: '/api/testing/payment/result'
 }
 
-/*
-const sampleUsers = [
+const samplePayments = [
 	{
-		email: 'tester@example.com',
-		name: 'John',
-		middlename: 'Tester',
-		lastname: 'Doe',
-		password: 'TestPassword3'
+		payment_id: '1411226302',
+		action: 'pay',
+		status: 'success',
+		version: 3,
+		type: 'buy',
+		paytype: 'card',
+		public_key: 'sandbox_i41231363208',
+		acq_id: 414963,
+		order_id: uuidv4(),
+		liqpay_order_id: 'N2J46LJU1598958249197033',
+		description: 'Оплата: лютий, березень, квітень. Викладач: Hank Hill. Учень: Bobby Hill. Предмет: Саксофон.',
+		sender_card_mask2: '414943*56',
+		sender_card_bank: 'pb',
+		sender_card_type: 'visa',
+		sender_card_country: 804,
+		ip: '130.180.212.73',
+		amount: 1,
+		currency: 'UAH',
+		sender_commission: 0,
+		receiver_commission: 0.03,
+		agent_commission: 0,
+		amount_debit: 1,
+		amount_credit: 1,
+		commission_debit: 0,
+		commission_credit: 0.03,
+		currency_debit: 'UAH',
+		currency_credit: 'UAH',
+		sender_bonus: 0,
+		amount_bonus: 0,
+		mpi_eci: 7,
+		language: 'uk',
+		create_date: 1598958230770,
+		end_date: 1598958251091,
+		transaction_id: 1411226302,
+		paymentDescr: ''
 	},
 	{
-		email: 'mary@example.com',
-		name: 'Mary',
-		middlename: 'Tester',
-		lastname: 'Doe',
-		password: 'TestPassword4'
+		payment_id: '1411226303',
+		action: 'pay',
+		status: 'success',
+		version: 3,
+		type: 'buy',
+		paytype: 'card',
+		public_key: 'sandbox_i41231363208',
+		acq_id: 414963,
+		order_id: uuidv4(),
+		liqpay_order_id: 'N2J46LJU1598958249197033',
+		description: 'Оплата: лютий, березень. Викладач: Hank Hill. Учень: Bobby Hill. Предмет: Piano.',
+		sender_card_mask2: '414943*56',
+		sender_card_bank: 'pb',
+		sender_card_type: 'visa',
+		sender_card_country: 804,
+		ip: '130.180.212.73',
+		amount: 2,
+		currency: 'UAH',
+		sender_commission: 0,
+		receiver_commission: 0.03,
+		agent_commission: 0,
+		amount_debit: 1,
+		amount_credit: 1,
+		commission_debit: 0,
+		commission_credit: 0.03,
+		currency_debit: 'UAH',
+		currency_credit: 'UAH',
+		sender_bonus: 0,
+		amount_bonus: 0,
+		mpi_eci: 7,
+		language: 'uk',
+		create_date: 1598958230770,
+		end_date: 1598958251091,
+		transaction_id: 1411226302,
+		paymentDescr: ''
 	}
 ]
 
 const validNonExistingId = async () => {
-	const user = new Payment({
-		...newUser,
-		email: 'temp@example.com',
-		teacher: null
-	})
-	await user.save()
-	await user.remove()
+	const [ payment ] = samplePayments
+	const description =
+		new PaymentDescr(getPaymentDataFromString(payment.description, 'uk-UA'))
+	description.save()
 
-	return user._id.toString()
+	const tempPayment = new Payment({
+		...payment,
+		payment_id: '1411226309',
+		paymentDescr: description.id
+	})
+	await tempPayment.save()
+	await tempPayment.remove()
+
+	return tempPayment._id.toString()
 }
 
-const usersInDb = async () => {
-	const users = await User.find({})
-	return users.map(user => user.toJSON())
-}*/
+const paymentsInDb = async () => {
+	const payments = await Payment.find({})
+	return payments.map(payment => payment.toJSON())
+}
 
 module.exports = {
-	// usersInDb,
+	paymentsInDb,
 	singlePayment,
-	// sampleUsers,
-	// validNonExistingId
+	samplePayments,
+	validNonExistingId
 }
